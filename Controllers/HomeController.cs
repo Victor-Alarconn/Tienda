e62e6959302dd5155a05d4411c6aeb99143e7f55
Victor <alarcongt4@gmail.com>
2023-10-10 10:39:12 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tienda.Data;
+using Tienda.Models;
+
+namespace Tienda.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DataConexion _dataConexion;
+
+        public HomeController()
+        {
+            _dataConexion = new DataConexion();
+        }
+
+        public ActionResult Categorias()
+        {
+            var categories = new List<Categoria>();
+            using (var connection = _dataConexion.CreateConnection())
+            {
+                connection.Open();
+                using (var command = new MySqlCommand("SELECT * FROM td_grupos", connection))
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        categories.Add(new Categoria
+                        {
+                            Id = reader.GetInt32("id_grupo"),
+                            Nombre = reader.GetString("td_nombre")
+                        });
+                    }
+                }
+            }
+
+            return View(categories);
+        }
+
+
+        public ActionResult Index()
+        {
+            var categories = new List<Categoria>();
+            using (var connection = _dataConexion.CreateConnection())
+            {
+                connection.Open();
+                using (var command = new MySqlCommand("SELECT * FROM td_grupos", connection))
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        categories.Add(new Categoria
+                        {
+                            Id = reader.GetInt32("id_grupo"),
+                            Nombre = reader.GetString("td_nombre")
+                        });
+                    }
+                }
+            }
+
+            return View(categories);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Data/DataConexion.cs
@@ -0,0 +1,25 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace Tienda.Data
+{
+    public class DataConexion
+    {
+        private readonly string _connectionString;
+
+        public DataConexion()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["MySqlConnectionString"].ToString();
+        }
+
+        public MySqlConnection CreateConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+    }
+}
