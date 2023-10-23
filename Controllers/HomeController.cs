673c744cd7e408762b39dbed5845ddd81e1f3f07
Victor <alarcongt4@gmail.com>
2023-10-23 17:54:37 -0500

    funciona

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,8 +2,11 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Transactions;
 using System.Web;
 using System.Web.Mvc;
 using Tienda.Data;
@@ -16,15 +19,15 @@ namespace Tienda.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly DataConexion _dataConexion;
-        private readonly Cart _cart;
-        public HomeController()
+        private readonly DataConexion _dataConexion; // Se crea una instancia de la clase DataConexion
+        private readonly Cart _cart; // Se crea una instancia de la clase Cart
+        public HomeController() // Constructor de la clase
         {
             _dataConexion = new DataConexion();
             _cart = new Cart();
         }
 
-        [HttpGet]
+        [HttpGet] // Acción para mostrar el formulario de pago
         public ActionResult Checkout()
         {
             var cart = Session["cart"] as Cart;
@@ -37,7 +40,7 @@ namespace Tienda.Controllers
             return View(model);
         }
 
-        [HttpPost]
+        [HttpPost] // Acción para procesar el pago y redirigir a PayU. 
         public ActionResult Checkout(PaymentInfo model)
         {
             StringBuilder productNames = new StringBuilder();
@@ -46,33 +49,32 @@ namespace Tienda.Controllers
             // Se prepara los datos para PayU
             var merchantId = "508029";
             var apiKey = "4Vj8eK4rloUd272L48hsrarnUA";
-            var referenceCode = "TestPayU-" + Guid.NewGuid().ToString();
+            var referenceCode = "TestPayU-" + Guid.NewGuid().ToString();  // Guardad en base de datos
             var amount = model.Cart.TotalPrice();
             var currency = "COP";
-            var buyerEmail = model.Email;
+            var buyerEmail = model.Email; // Guardad en base de datos
             var accountId = "512321";
             var tax = 0;
             var taxReturnBase = 0;
             var test = "1";
-            var phone = model.Phone;
+            var phone = model.Phone; // Guardad en base de datos
             var fullName = $"{model.FirstName} {model.LastName}";  // Combina FirstName y LastName
-            var address = model.StreetAddress;
-            var document = model.DocumentType;
-            var documentNumber = model.DocumentNumber;
-            var company = model.CompanyName;
-            var country = model.Country;
-            var state = model.State;
-            var city = model.City;
-            var PostalCode = model.postalCode;
+            var address = model.StreetAddress; // Guardad en base de datos
+            var document = model.DocumentType; // Guardad en base de datos
+            var documentNumber = model.DocumentNumber; // Guardad en base de datos
+            var company = model.CompanyName; // Guardad en base de datos
+            var country = model.Country; // Guardad en base de datos
+            var state = model.State; // Guardad en base de datos
+            var city = model.City; // Guardad en base de datos
+            var PostalCode = model.postalCode; // Guardad en base de datos
             var paymentMethod = "VISA,VISA_DEBIT,PSE,MASTERCARD,MASTERCARD_DEBIT";
-
             var responseUrl = "http://localhost:5000/Home/Confirmation";
             var confirmationUrl = "http://localhost:5000/Home/Confirmation";
             // Genera la firma
             var formattedAmount = amount.ToString("F2", System.Globalization.CultureInfo.InvariantCulture);
             var signature = GenerarFirma(apiKey, merchantId, referenceCode, formattedAmount, currency);  // Llamada un nuevo método
 
-            foreach (var item in model.Cart.Items)
+            foreach (var item in model.Cart.Items) // Guardad todos los productos en base de datos 
             {
                 if (productNames.Length > 0)
                 {
@@ -81,6 +83,27 @@ namespace Tienda.Controllers
                 productNames.Append(item.Product.Nombre);
             }
             string productDescription = productNames.ToString();
+            using (var connection = _dataConexion.CreateConnection())
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        int userId = UsuarioInfo(buyerEmail, phone, fullName, address, document, documentNumber, company, country, state, city, PostalCode, connection, transaction);
+                        int orderId = Orden(userId, referenceCode, amount, productDescription, connection, transaction);
+                        GuardarProductos(orderId, model.Cart, connection, transaction);
+
+                        // Si todo ha ido bien
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                        throw;  // Puedes manejar el error como prefieras o informar al usuario.
+                    }
+                }
+            }
             // Se crea un nuevo modelo para pasar a la vista de PayU
             var payUModel = new PayUModel
             {
@@ -106,6 +129,7 @@ namespace Tienda.Controllers
             return View("PayUForm", payUModel);
         }
 
+        // Método para generar la firma de la solicitud
         private string GenerarFirma(string apiKey, string merchantId, string referencia, string precio, string currency)
         {
             var datos = $"{apiKey}~{merchantId}~{referencia}~{precio}~{currency}";
@@ -123,29 +147,114 @@ namespace Tienda.Controllers
             }
         }
 
-       
-            // Acción para la respuesta que muestra una vista al usuario
-            [HttpPost]
-            public ActionResult PayUResponse(PayUResponse model)
-            {
-                // Aquí puedes procesar alguna lógica si es necesario.
-                return View(model);
-            }
 
-            // Acción para la confirmación que solo procesa datos en backend y no muestra una vista
-            [HttpPost]
-            public ActionResult Confirmation(PayUConfirmation model)
-            {
-                // Procesar los datos (guardar en la base de datos, etc.)
-                // ...
-
-                // Devolver una respuesta HTTP indicando que todo está bien.
-                return View();
-            }
-        
-
-
+        // Acción para la respuesta que muestra una vista al usuario
         [HttpPost]
+        public ActionResult PayUResponse(PayUResponse model)
+        {
+            string apiKey = "4Vj8eK4rloUd272L48hsrarnUA"; // Reemplaza con tu API key
+            string generatedSignature = CreateSignature(apiKey, model.MerchantId, model.ReferenceCode, Convert.ToDecimal(model.TX_VALUE), model.Currency, Convert.ToInt32(model.TransactionState));
+
+            if (generatedSignature.Equals(model.Signature, StringComparison.OrdinalIgnoreCase))
+            {
+                // La firma es válida
+            }
+            else
+            {
+                // La firma no es válida
+            }
+
+            return View(model);
+        }
+
+        // Método para crear la firma de confirmación
+        private string CreateSignature(string apiKey, string merchantId, string referenceCode, decimal txValue, string currency, int transactionState)
+        {
+            // Aproxima TX_VALUE a un decimal usando el método de redondeo Round half to even
+            decimal newValue = Math.Round(txValue, 1, MidpointRounding.ToEven);
+            // Genera la cadena para la firma
+            string rawSignature = $"{apiKey}~{merchantId}~{referenceCode}~{newValue}~{currency}~{transactionState}";
+            // Calcula el hash MD5
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(rawSignature));
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }
+
+        private int UsuarioInfo(string buyerEmail, int phone, string fullName, string address, string document, string documentNumber, string company, string country, string state, string city, int PostalCode, MySqlConnection connection, MySqlTransaction transaction)
+        {
+            // Definir la consulta SQL para insertar la información del usuario en la base de datos.
+            string query = @"INSERT INTO td_user(email, telef, nombre, direc, tipo_doc, numer_doc, nomb_empr, pais, depart, city, postalnum) VALUES (@Email, @Phone, @FullName, @Address, @DocumentType, @DocumentNumber, @CompanyName, @Country, @State, @City, @PostalCode);
+                                 SELECT LAST_INSERT_ID();";
+
+            using (var command = new MySqlCommand(query, connection, transaction))
+            {
+                // Asignar valores a los parámetros de la consulta SQL.
+                command.Parameters.AddWithValue("@Email", buyerEmail);
+                command.Parameters.AddWithValue("@Phone", phone);
+                command.Parameters.AddWithValue("@FullName", fullName);
+                command.Parameters.AddWithValue("@Address", address);
+                command.Parameters.AddWithValue("@DocumentType", document);
+                command.Parameters.AddWithValue("@DocumentNumber", documentNumber);
+                command.Parameters.AddWithValue("@CompanyName", company);
+                command.Parameters.AddWithValue("@Country", country);
+                command.Parameters.AddWithValue("@State", state);
+                command.Parameters.AddWithValue("@City", city);
+                command.Parameters.AddWithValue("@PostalCode", PostalCode);
+
+                // Ejecutar la consulta SQL.
+                return Convert.ToInt32(command.ExecuteScalar()); // Devuelve el ID del usuario recién creado.
+            }
+            
+        }
+
+        private int Orden(int userId, string referenceCode, decimal amount, string productDescription, MySqlConnection connection, MySqlTransaction transaction)
+        {
+            // Ahora, además de los otros campos, también insertamos el estado en la consulta SQL.
+            string orderInsertQuery = @"INSERT INTO td_orden(User_Id, refere, total, descrip, td_estado)
+                    VALUES (@UserId, @ReferenceCode, @Amount, @ProductDescription, @Estado);
+                    SELECT LAST_INSERT_ID();"; // Devuelve el ID recién creado.
+
+            using (var command = new MySqlCommand(orderInsertQuery, connection, transaction))
+            {
+                command.Parameters.AddWithValue("@UserId", userId);
+                command.Parameters.AddWithValue("@ReferenceCode", referenceCode);
+                command.Parameters.AddWithValue("@Amount", amount);
+                command.Parameters.AddWithValue("@ProductDescription", productDescription);
+                command.Parameters.AddWithValue("@Estado", 1); // estado de la transacción
+
+                return Convert.ToInt32(command.ExecuteScalar()); // Devuelve el ID de la orden recién creada.
+            }
+        }
+
+
+
+        private void GuardarProductos(int orderId, Cart cart, MySqlConnection connection, MySqlTransaction transaction)
+        {
+
+            foreach (var item in cart.Items)
+                {
+                 string orderProductInsertQuery = "INSERT INTO td_produc(OrderId, ProductId, cantidad) VALUES (@OrderId, @ProductId, @Quantity)";
+
+                using (var command = new MySqlCommand(orderProductInsertQuery, connection, transaction))
+                {
+                    command.Parameters.AddWithValue("@OrderId", orderId);
+                    command.Parameters.AddWithValue("@ProductId", item.Product.Id);  
+                    command.Parameters.AddWithValue("@Quantity", item.Quantity); 
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+
+        [HttpPost] // Accion para agregar un producto al carrito
         public ActionResult AddToCart()
         {
             try
@@ -200,7 +309,7 @@ namespace Tienda.Controllers
             }
         }
 
-        [HttpPost]
+        [HttpPost] // Acción para eliminar un producto del carrito
         public ActionResult RemoveFromCart(int productId)
         {
             // Obtener el carrito de la sesión
@@ -230,7 +339,7 @@ namespace Tienda.Controllers
         }
 
 
-        public ActionResult Carrito()
+        public ActionResult Carrito() // Acción para mostrar el carrito
         {
             var cart = Session["cart"] as Cart;
             if (cart == null)
@@ -241,7 +350,7 @@ namespace Tienda.Controllers
             return View(cart);
         }
 
-        [HttpPost]
+        [HttpPost] // Acción para actualizar la cantidad de un producto en el carrito
         public JsonResult UpdateQuantity(int productId, int quantity)
         {
             var cart = Session["cart"] as Cart;
@@ -263,7 +372,7 @@ namespace Tienda.Controllers
         }
 
 
-        private Producto GetProductById(int productId)
+        private Producto GetProductById(int productId) // Método para obtener un producto por su ID
         {
             using (var connection = _dataConexion.CreateConnection())
             {
@@ -298,7 +407,7 @@ namespace Tienda.Controllers
             return null;
         }
 
-        public ActionResult CategoryNav()
+        public ActionResult CategoryNav() // Acción para mostrar las categorías en la barra de navegación
         {
             var categories = new List<Categoria>();
             using (var connection = _dataConexion.CreateConnection())
@@ -320,7 +429,7 @@ namespace Tienda.Controllers
             return PartialView("_CategoryNav", categories);
         }
 
-        public ActionResult Index(int? categoryId)
+        public ActionResult Index(int? categoryId) // Accion para mostrar los productos en la página principal
         {
             var products = new List<Producto>();
             using (var connection = _dataConexion.CreateConnection())
@@ -364,7 +473,7 @@ namespace Tienda.Controllers
             return View(products);
         }
 
-        public ActionResult Details(int id)
+        public ActionResult Details(int id) // Acción para mostrar los detalles de un producto
         {
             Producto product = null;
             List<Producto> relatedProducts;
@@ -437,27 +546,28 @@ namespace Tienda.Controllers
             return View(viewModel);
         }
 
-        public ActionResult About()
+        public ActionResult About() // aun sin implementar
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contact() // aun sin implementar
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
 
-        public ActionResult Politica()
+        public ActionResult Politica() // Acción para mostrar la política de privacidad
         {
 
             return View();
         }
+       
         
-        public ActionResult Confirmation()
+        public ActionResult Confirmation() // Acción para mostrar la confirmación de la compra
         {
 
             return View();
--- Models/PayUResponse.cs
+++ Models/PayUResponse.cs
@@ -12,5 +12,12 @@ namespace Tienda.Models
         public string Amount { get; set; }
         public string Description { get; set; }
         public bool IsSuccess { get; set; }
+        public string MerchantId { get; set; }
+        public string ReferenceCode { get; set; }
+        public string TX_VALUE { get; set; }
+        public string Currency { get; set; }
+        public string TransactionState { get; set; }
+        public string Signature { get; set; }
+
     }
 }
