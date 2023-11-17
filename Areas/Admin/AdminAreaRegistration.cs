b15342fd2737c56c85ad21afbeacdfbd559da1e8
Victor <alarcongt4@gmail.com>
2023-11-17 16:58:10 -0500

    se agrega area de admin

---
+++ Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Tienda.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new { action = "Verificacion", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
