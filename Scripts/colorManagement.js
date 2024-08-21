3621ddbd3ff3b2a5f433e91aa10faeaecee3919b
ema <emanuel.garcia@utp.edu.co>
2024-08-21 17:32:02 -0500

    Se agrego color predeterminado pagina

---
+++ Scripts/colorManagement.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    var defaultColor = "#808080";
+
+    // Configuración del color picker para la barra de navegación
+    $("#navbarColorPicker").spectrum({
+        color: defaultColor,
+        showInput: true,
+        preferredFormat: "hex",
+        showPalette: true,
+        palette: [
+            ["#007BFF", "#FF0000", "#86B038"]
+        ],
+        change: function (color) {
+            var selectedColor = color.toHexString();
+            document.getElementById('navbar').style.backgroundColor = selectedColor;
+            localStorage.setItem('navbarColor', selectedColor);
+        }
+    });
+
+    // Configuración del color picker para el slider
+    $("#sliderColorPicker").spectrum({
+        color: defaultColor,
+        showInput: true,
+        preferredFormat: "hex",
+        showPalette: true,
+        palette: [
+            ["#007BFF", "#FF0000", "#d6f792"]
+        ],
+        change: function (color) {
+            var selectedColor = color.toHexString();
+            document.querySelector('.sb-sidenav-menu').style.backgroundColor = selectedColor;
+            localStorage.setItem('sliderColor', selectedColor);
+        }
+    });
+
+    // Aplicar colores guardados o gris predeterminado
+    var savedNavbarColor = localStorage.getItem('navbarColor') || defaultColor;
+    document.getElementById('navbar').style.backgroundColor = savedNavbarColor;
+    $("#navbarColorPicker").spectrum("set", savedNavbarColor);
+
+    var savedSliderColor = localStorage.getItem('sliderColor') || defaultColor;
+    document.querySelector('.sb-sidenav-menu').style.backgroundColor = savedSliderColor;
+    $("#sliderColorPicker").spectrum("set", savedSliderColor);
+});
