b1826e82d1fad77d72a02013843ef192b293c043
Victor_Alarcon <85529967+Victor-Alarconn@users.noreply.github.com>
2024-08-21 09:19:19 -0500

    nose que se daño

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace Tienda
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/complementos").Include(
+            "~/Scripts/fontawesome/all.min.js",
+            "~/Scripts/scripts.js"));
+
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
